8f8c922c6670e91047587a07c3e00e39addc5e61
Konrad <konrads0705@wp.pl>
2023-06-13 22:34:21 +0200

    Done

---
+++ src/main/java/efs/task/reflection/annotations/NotNull.java
@@ -0,0 +1,8 @@
+package efs.task.reflection.annotations;
+
+import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.PARAMETER})
+public @interface NotNull {
+    String value() default "n/a";
+}
